6f2e4bae5cbef17bf73145388b756465bb52cd76
Hector Salas Serrano <kenkira666@gmail.com>
2023-07-27 08:19:46 -0500

    declared done

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1  // Exercise 2
+const firstName = "Hector";
+let lastName = "Salas";
+var age = 37;
+const message = ("My name is ${firstName} ${lastName} and i am ${age}");
+console.log(message)
 
 // Exercise 3
+let lenguage = 'JavaScript';
+let createdYear = 1995;
+let isCaseSentitive = true;
+
+// exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedZise = "M";
+
+//exercise 5
+let title = "Name Of The Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
